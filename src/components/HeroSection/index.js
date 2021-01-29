db72b133b2fd2c384afdb002f26d156f6d73f0c2
Avinash Joshi <avinash.joshi@selu.edu>
2021-01-29 14:58:54 -0600

    Added intro video

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { HeroContainer, HeroBg, VideoBg } from "./HeroElements";
+import Video from "../../videos/video.mp4";
 
 const HeroSection = () => {
   return (
@@ -6,6 +8,15 @@ const HeroSection = () => {
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Hi, I'm Avinash.</HeroH1>
+        <HeroP>I'm a Software Developer, ready to be your new hire.</HeroP>
+        <HeroBtnWrapper>
+          <Button to='/contact'>
+            Hire Me! {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
